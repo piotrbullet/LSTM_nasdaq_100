302d7e0a10f605092e8d38ed74491eff8e6ae3af
Piotr <piotrbullet69@gmail.com>
2024-02-17 18:23:00 +0100

    initial commit

---
+++ get_index.py
@@ -0,0 +1,22 @@
+import yfinance as yf
+import pandas as pd
+from datetime import datetime, timedelta
+from enum import Enum
+
+
+
+def download_nasdaq_100_data(start_date, end_date, interval):
+    nasdaq_minute_data = yf.download('^NDX', start=start_date, end=end_date, interval=interval)
+    nasdaq_minute_data.to_csv(f'data\\{interval}\\{end_date}.csv')
+    
+if __name__ == "__main__":
+    days_back = 7
+    interval = "1m"
+    end_date = datetime.now().strftime(r'%Y-%m-%d')
+    
+    if interval == "1m" and days_back > 7:
+        days_back = 7
+    
+    start_date = (datetime.now() - timedelta(days=days_back)).strftime(r'%Y-%m-%d')
+    
+    download_nasdaq_100_data(start_date, end_date, "1m")
