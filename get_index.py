8644a85202355f2c0794100ef73658d99713c1a3
Piotr <piotrbullet69@gmail.com>
2024-02-17 18:30:01 +0100

    2

--- get_index.py
+++ get_index.py
@@ -2,17 +2,24 @@ import yfinance as yf
 import pandas as pd
 from datetime import datetime, timedelta
 from enum import Enum
-
+import os
 
 
 def download_nasdaq_100_data(start_date, end_date, interval):
     nasdaq_minute_data = yf.download('^NDX', start=start_date, end=end_date, interval=interval)
+    
     nasdaq_minute_data.to_csv(f'data\\{interval}\\{end_date}.csv')
     
 if __name__ == "__main__":
     days_back = 7
     interval = "1m"
     end_date = datetime.now().strftime(r'%Y-%m-%d')
+    
+    if not os.path.exists("data"):
+        os.mkdir("data")
+        
+    if not os.path.exists(f"data\\{interval}"):
+        os.mkdir(f"data\\{interval}")
     
     if interval == "1m" and days_back > 7:
         days_back = 7
